d40da1993dba2956d71af19f0c29ae2c3a496e79
lsp8 <lahude.paulo@gmail.com>
2023-03-17 11:11:47 -0300

    funcionando exibindo resposta de uma vez

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { Configuration, OpenAIApi } from "openai";
 
-function App() {
+import FormSection from "./components/FormSection";
+import AnswerSection from "./components/AnswerSection";
+
+import { useState } from "react";
+
+const App = () => {
+  const configuration = new Configuration({
+    apiKey: process.env.REACT_APP_OPENAI_API_KEY,
+  });
+
+  const openai = new OpenAIApi(configuration);
+
+  const [storedValues, setStoredValues] = useState([]);
+
+  const generateResponse = async (newQuestion, setNewQuestion) => {
+    let options = {
+      model: "text-davinci-003",
+      temperature: 0,
+      max_tokens: 2048,
+      top_p: 1,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
+      stop: ["/"],
+    };
+
+    let completeOptions = {
+      ...options,
+      prompt: newQuestion,
+    };
+
+    const response = await openai.createCompletion(completeOptions);
+
+    if (response.data.choices) {
+      setStoredValues([
+        {
+          question: newQuestion,
+          answer: response.data.choices[0].text,
+        },
+        ...storedValues,
+      ]);
+      setNewQuestion("");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <div className="header-section">
+        <h1>Assistente PanvelLabs - by Paulo Salim</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Olá!Seja bem vindo ao sistema de perguntas e respostas do Panvel Labs!
+          Sou um sistema automatizado, desenvolvido para lhe auxiliar a
+          encontrar informações relevantes. Sinta-se a vontade para realizar
+          qualquer pergunta e eu farei o meu melhor para trazer informações
+          confiáveis.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+
+      <FormSection generateResponse={generateResponse} />
+
+      <AnswerSection storedValues={storedValues} />
     </div>
   );
-}
+};
 
 export default App;
